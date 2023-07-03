b51fb37a38612379b0e388efa3b164430060a69b
“Zhenya” <dyadicheve@mail.ru>
2023-07-03 21:56:36 +0300

    testPassed

--- src/test/java/ru/netology/postmanechotest/PostmanEchoTest.java
+++ src/test/java/ru/netology/postmanechotest/PostmanEchoTest.java
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Bye"))
+                .body("data", equalTo("Hi"))
         ;
     }
 
