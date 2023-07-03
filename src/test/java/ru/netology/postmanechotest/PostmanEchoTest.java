3859b69dbbebd3497b0845d8cf1ff87f9759d9d3
“Zhenya” <dyadicheve@mail.ru>
2023-07-03 21:53:47 +0300

    testFailed

---
+++ src/test/java/ru/netology/postmanechotest/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology.postmanechotest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+
+    void shouldReturnSendData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Bye"))
+        ;
+    }
+
+}
